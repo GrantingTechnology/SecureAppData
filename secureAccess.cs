19113ab935c67bc392276e8fc35701091b3841de
Granting Technology - GT (Community) <30596361+GrantingTechnology@users.noreply.github.com>
2019-04-20 12:21:17 +0200

    Inclusion of main files folder App_Data
    Files for inclusion in the App_Data folder

---
+++ secureAccess.cs
@@ -0,0 +1,28 @@
+﻿using gt.business.log;
+using System;
+using System.Web.Mvc;
+
+namespace gt.community.secure
+{
+    public class SecureAccess : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            try
+            {
+
+                base.OnActionExecuting(filterContext);
+
+            }
+            catch (Exception e)
+            {
+
+                Register.Log(this, ref e);
+            }
+
+        }
+
+    }
+}
---
+++ secureUtility.cs
@@ -0,0 +1,57 @@
+﻿using gt.business.log;
+using gt.enumerator.general;
+using System;
+using System.Web.Mvc;
+//using ...add here the business namespace for the user interface
+
+namespace gt.community.secure
+{
+    
+    public class Utility : Controller
+    {
+
+        #region attribute
+        internal run gt;
+        public string _pathsecure = string.Empty;
+        internal bool isAuthenticate = false;
+        //internal login_identity infoUser = new login_identity();
+        private Invoke.Run.Page _invokepageCurrent = Invoke.Run.Page.indifferent;
+        #endregion
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            #region run
+            Injected();
+            #endregion
+        }
+
+        public Utility()
+        {
+
+#if !DEBUG
+               _pathsecure = "/Secure" ;          
+#endif
+
+        }
+
+        /// <summary>
+        /// Internal method for population and container customization of gt.business framework [layer .. [current]]
+        /// </summary>
+        internal void Injected()
+        {
+            gt.HeadPage(_invokepageCurrent);
+            ViewBag.Title = gt.Title;
+            ViewBag.MetaDescription = gt.MetaDescription;
+            ViewBag.MetaRobots = gt.metaRobot;
+            ViewBag.pathRelactive = _pathsecure;
+            //ViewBag.gt_headlink = gt.gt_headlink;
+            //ViewBag.gt_headscript = gt.gt_headscript;
+            //ViewBag.gt_footercript = gt.gt_footercript;
+            ViewBag.toAlert = string.Empty;
+        }
+
+    }
+
+
+}
