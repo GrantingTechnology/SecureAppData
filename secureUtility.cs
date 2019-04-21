77c744286171d0fb69606bb3cf37ed2d9db7d959
Granting Technology - GT (Community) <30596361+GrantingTechnology@users.noreply.github.com>
2019-04-21 07:13:10 -0300

    change in script version
    update

--- secureUtility.cs
+++ secureUtility.cs
@@ -1,4 +1,4 @@
-﻿using gt.business.log;
+using gt.business.log;
 using gt.enumerator.general;
 using System;
 using System.Web.Mvc;
@@ -9,7 +9,6 @@ namespace gt.community.secure
     
     public class Utility : Controller
     {
-
         #region attribute
         internal run gt;
         public string _pathsecure = string.Empty;
@@ -22,6 +21,7 @@ namespace gt.community.secure
         {
             base.OnActionExecuting(filterContext);
             #region run
+            Authenticate();
             Injected();
             #endregion
         }
@@ -48,10 +48,58 @@ namespace gt.community.secure
             //ViewBag.gt_headlink = gt.gt_headlink;
             //ViewBag.gt_headscript = gt.gt_headscript;
             //ViewBag.gt_footercript = gt.gt_footercript;
+            //ViewBag.gt_solutionConfiguration = Debugger.IsAttached ? "debug" : "production";
             ViewBag.toAlert = string.Empty;
         }
 
+        /// <summary>
+        /// Get user value, items restricted to user current
+        /// </summary>
+        /// <param name="authenticData">Object type: String, json data represents</param>
+        public void getInfo(string authenticData)
+        {
+            if (authenticData != null)
+                if (authenticData != string.Empty)
+                {
+                    ModelCollection modelcollection = new ModelCollection();
+                    modelcollection.AddModel(authenticData);
+                    gt.controlPopulation(ref modelcollection, invokeRoutine.sel_adm_user_authenticate);
+                }
+        }
+
+
+        /// <summary>
+        /// Security status manager, current user information
+        /// </summary>
+        private void Authenticate()
+        {
+            try
+            {
+                ViewBag.padlock = Settings.gt_secure_img_padlock;
+                if (System.Web.HttpContext.Current.Session[Settings.gt_ticketAccess] != null)
+                {
+                    isAuthenticate = (System.Web.HttpContext.Current.Session[Settings.gt_ticketAccess] == null ? false : true);
+                    if (gt.UserName == string.Empty)
+                    {
+                        ModelCollection modelcollection = new ModelCollection();
+                        modelcollection.AddModel(infoUser);
+                        modelcollection.AddModel((System.Web.HttpContext.Current.Session[Settings.gt_ticketAccess].ToString()));
+                        gt.controlPopulation(ref modelcollection, invokeRoutine.sel_adm_user_authenticate);
+                    }
+                }
+
+                if (isAuthenticate)
+                {
+                    ViewBag.padlock = Settings.gt_secure_img_padlock_unlock;
+                    ViewBag.UserName = infoUser.name;
+                    ViewBag.UserPermission = infoUser.permission_nick;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Register.Log(this, ref ex);
+            }
+        }
     }
-
-
 }
